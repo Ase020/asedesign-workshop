d5584315c2b1894cbc335ec27d2bc812cc4ef538
felixNyalenda <felixasejunior@gmail.com>
2023-06-01 22:18:24 +0300

    contact section build

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,41 @@
+/* eslint-disable react/no-unescaped-entities */
+import Image from "next/image";
 import styles from "./page.module.css";
+import { Button } from "@/components";
+
+export const data = {
+  title: "aseDesign Contact Information",
+  description: "This is Contact Page",
+};
 
 const Contact = () => {
-  return <div className={styles.container}>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.image}
+            src="/contact.png"
+            alt="contact"
+            fill={true}
+          />
+        </div>
+
+        <form className={styles.form}>
+          <input type="text" placeholder="Name" className={styles.input} />
+          <input type="text" placeholder="Email" className={styles.input} />
+          <textarea
+            className={styles.textarea}
+            placeholder="Message"
+            cols="30"
+            rows="10"
+          ></textarea>
+          <Button text="Send" url="#" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
