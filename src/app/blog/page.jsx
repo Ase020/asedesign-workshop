5ab6d38b1259adc9b96c4fbc24d00b43bde0e74a
felixNyalenda <felixasejunior@gmail.com>
2023-06-04 19:50:03 +0300

    Digest: 2026204822

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -19,24 +19,25 @@ const Blog = async () => {
   const data = await getData();
   return (
     <div className={styles.mainContainer}>
-      {data.map(({ _id, title, desc, image }) => (
-        <Link href={`blog/${_id}`} className={styles.container} key={_id}>
-          <div className={styles.imageContainer}>
-            <Image
-              src={image}
-              alt=""
-              width={400}
-              height={250}
-              className={styles.image}
-            />
-          </div>
+      {data &&
+        data?.map(({ _id, title, desc, image }) => (
+          <Link href={`blog/${_id}`} className={styles.container} key={_id}>
+            <div className={styles.imageContainer}>
+              <Image
+                src={image}
+                alt=""
+                width={400}
+                height={250}
+                className={styles.image}
+              />
+            </div>
 
-          <div className={styles.content}>
-            <h1 className={styles.title}>{title}</h1>
-            <p className={styles.desc}>{desc}</p>
-          </div>
-        </Link>
-      ))}
+            <div className={styles.content}>
+              <h1 className={styles.title}>{title}</h1>
+              <p className={styles.desc}>{desc}</p>
+            </div>
+          </Link>
+        ))}
     </div>
   );
 };
